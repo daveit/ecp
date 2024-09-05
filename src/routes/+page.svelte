8ed3ec07a5a628c6d788190a3c5630892a16701c
Dave Porter <daveporter@me.com>
2024-09-05 15:15:16 +0800

    Add existing project files to Git

---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		Welcome to Safe-T-Products Utilities
+	</h1>
+	<img src="stp_logo_650w.png" alt="Welcome logo" />
+
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+		padding-bottom: 30px;
+	}
+
+/*	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+*/
+img {
+		width: 650px;
+		top: 0;
+		display: block;
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,51 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<main>
+	<div class="container">
+		<h1>About Safe-T-Products</h1>
+		<p>
+			Safe-T-Products, established in 1991, is a global leader in designing and manufacturing a
+			diverse range of safety equipment, including emergency stops, protective stop controls, and
+			various safety solutions originally developed for conveyor systems and now extending to other
+			safety systems and machinery.
+		</p>
+		<p>
+			Under the guidance of Managing Director Marcus Coyle, the company has expanded its global reach
+			and continues to pioneer advancements in safety technology. Based in Perth, Western Australia,
+			Safe-T-Products benefits from close proximity to major mining operations, fostering strong
+			industry partnerships and enabling real-world testing of its products.
+		</p>
+		<p>
+			Dedicated to sustainability and excellence, the company heavily invests in research and
+			development to ensure its devices meet the highest global standards. We invite you to explore
+			our website to discover our extensive range of safety devices tailored to meet your needs. With
+			a deeply ingrained safety culture, Safe-T-Products is well-positioned for continued growth and
+			innovation, making mining safer through superior design and manufacturing.
+		</p>
+	</div>
+</main>
+
+<!-- <h1 class="font-semibold text-2xl">The About Page</h1>
+<p class="text-xl">Welcome</p>
+ -->
+
+<style>
+    .container {
+        width: 65%;
+        margin: var(--column-margin-top) auto 0 auto;
+    }
+    h1 {
+        text-align: left;
+        font-size: 2.5rem;
+        font-weight: bold;
+        padding-bottom: 2rem;
+    }
+    p {
+        font-size: 1.1rem;
+        line-height: 32px;   /* within paragraph */
+        margin-bottom: 30px; /* between paragraphs */
+    }
+</style>
